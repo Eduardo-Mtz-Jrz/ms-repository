a3cc97228c8d34112a7a1a30380b2db013362911
DEIVIDS <david.cadena@ids.com.mx>
2026-02-17 16:45:03 -0600

    test

--- src/test/java/com/ms_products/ms_products/entity/ProductEntityTest.java
+++ src/test/java/com/ms_products/ms_products/entity/ProductEntityTest.java
@@ -1,8 +1,24 @@
 package com.ms_products.ms_products.entity;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
+@SpringBootTest
 class ProductEntityTest {
+
+    private ProductEntity productEntity;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1","a"})
+    void idTestValid(String values){
+        assertThrows(NullPointerException.class, () -> {
+            this.productEntity.setId(Long.valueOf(values));
+        });
+    }
 
 }
