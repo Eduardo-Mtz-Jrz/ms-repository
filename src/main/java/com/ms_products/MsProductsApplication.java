1806cad636ead40e278b7042469174308811cded
Eduardo MArtinez Juarez <eduard.maju@gmail.com>
2026-02-18 15:29:22 -0600

    circuit braker

---
+++ src/main/java/com/ms_products/MsProductsApplication.java
@@ -0,0 +1,13 @@
+package com.ms_products.ms_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients // <--- Agrega esta anotación
+public class MsProductsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ms_products/MsProductsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ms_products.ms_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
